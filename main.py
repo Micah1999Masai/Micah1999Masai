0c2b6721f307e9663d65406a195536472d5b9cbf
micah masai kipkirui <103409704+Micah1999Masai@users.noreply.github.com>
2022-12-13 14:44:27 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def endprogram():
 
 #Reading the image by parsing the argument 
 file_uploaded = st.file_uploader('Choose an image...', type = 'jpg')
-    if file_uploaded is not None :
+if file_uploaded is not None :
         image = Image.open(file_uploaded)
 	text = str(file_uploaded)
 	print ("\n*********************\nImage : " + ImageFile + "\n*********************")
