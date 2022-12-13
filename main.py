5996ef28349c0ef2d90d0e150e38269915b89a4e
micah masai kipkirui <103409704+Micah1999Masai@users.noreply.github.com>
2022-12-13 14:55:13 +0300

    Update main.py

--- main.py
+++ main.py
@@ -13,7 +13,7 @@ def endprogram():
 #Reading the image by parsing the argument 
 file_uploaded = st.file_uploader('Choose an image...', type = 'jpg')
 if file_uploaded is not None :
-	image = Image.open(file_uploaded)
+	img = image.open(file_uploaded)
 	text = str(file_uploaded)		
 	img = cv2.imread(text)
 	print ("\n*********************\nImage : " + ImageFile + "\n*********************")
