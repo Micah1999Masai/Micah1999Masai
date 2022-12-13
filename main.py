6caca04f7acdb4c6f14d7c3ab0d0a70967d26bbc
micah masai kipkirui <103409704+Micah1999Masai@users.noreply.github.com>
2022-12-13 15:43:47 +0300

    Update main.py

--- main.py
+++ main.py
@@ -19,9 +19,9 @@ if file_uploaded is not None :
 	img = cv2.imread(text)
 	#print ("\n*********************\nImage : " + file_uploaded + "\n*********************")
 	#img = cv2.resize(img ,((int)(img.shape[1]/5),(int)(img.shape[0]/5)))
-	neworiginal = img.copy()
-	cv2.imshow('original',img)
-	p = 0
+	#neworiginal = img.copy()
+	#cv2.imshow('original',img)
+	#p = 0
 	
 #Calculating number of pixels with shade of white(p) to check if exclusion of these pixels is required or not (if more than a fixed %) in order to differentiate the white background or white patches in image caused by flash, if present.
 #for i in range(img.shape[0]):	
