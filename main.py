55b97b389740de13f6efc89ad33d8ceb406e6636
micah masai kipkirui <103409704+Micah1999Masai@users.noreply.github.com>
2022-12-13 14:38:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,18 +11,16 @@ def endprogram():
 
 #Reading the image by parsing the argument 
 
-
-def main() :
-    file_uploaded = st.file_uploader('Choose an image...', type = 'jpg')
-    if file_uploaded is not None :
-		img = Image.open(file_uploaded)
-text = str(img)
-original = img.copy()
+text = str(ImageFile)
+print ("\n*********************\nImage : " + ImageFile + "\n*********************")
 img = cv2.imread(text)
+
 img = cv2.resize(img ,((int)(img.shape[1]/5),(int)(img.shape[0]/5)))
-neworiginal = img.copy()
+original = img.copy()
+neworiginal = img.copy() 
 cv2.imshow('original',img)
-	
+
+
 #Calculating number of pixels with shade of white(p) to check if exclusion of these pixels is required or not (if more than a fixed %) in order to differentiate the white background or white patches in image caused by flash, if present.
 p = 0 
 for i in range(img.shape[0]):
